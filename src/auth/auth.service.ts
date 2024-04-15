9758fb24c51043c009d00e6dc9d81ea8f639f48e
atuleshkarna <atulesh@progressivelabs.tech>
2024-04-15 21:06:48 +0545

    implementation of jwt and extraction of access_key for signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
+import { JwtService } from '@nestjs/jwt';
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private config: ConfigService,
+  ) {}
   async signup(userData: AuthDto) {
     const hash = await argon.hash(userData.password);
     try {
@@ -31,5 +37,31 @@ export class AuthService {
       }
       throw error;
     }
+  }
+
+  async signin(userData: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: userData.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const pwMatch = await argon.verify(user.hash, userData.password);
+    if (!pwMatch) throw new ForbiddenException('Credentials incorrect');
+    delete user.hash;
+
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      userName: `${user.firstName} ${user.lastName}`,
+    };
+    const secret = this.config.get('JWT_SECRET');
+    return {
+      access_token: await this.jwtService.signAsync(payload, {
+        expiresIn: '48h',
+        secret: secret,
+      }),
+    };
   }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          // Todo: use env,
-          url: 'postgresql://postgres:admin@localhost:5434/blog?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
